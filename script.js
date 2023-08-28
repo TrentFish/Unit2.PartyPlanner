961335de31e008e22467650cb70d7d0d94ec7f65
fishyboi@MSI <fishyboi@MSI>
2023-08-28 14:55:23 -0400

    API rendered successfully

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 let parties = [];
+const partyList = document.querySelector('#partyList')
 
 async function getParties(){
     const response = await fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2307-ftb-et-web-ft/events');
     const jason = await response.json();
     parties = jason.data;
-    //render();
-    console.log(parties);
+    render();
 }
 
+function render(){
+    const html = parties.map(function(party){
+        console.log(party);
+        return`
+        <div>
+          <h2>${party.name}</h2>
+          <p>Location: ${party.location}</p>
+          <p>Date & Time: ${party.date}</p>
+          <p>Description: ${party.description}</p>
+        </div>
+        `
+    }).join('');
+    partyList.innerHTML = html;
+}
 
 getParties();
