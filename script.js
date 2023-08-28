cb7f32cf37520f50314b011e8d59ad46083ed0ac
fishyboi@MSI <fishyboi@MSI>
2023-08-28 14:37:30 -0400

    Async function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello There");
+let parties = [];
+
+async function getParties(){
+    const response = await fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2307-ftb-et-web-ft/events');
+    const jason = await response.json();
+    parties = jason.data;
+    //render();
+    console.log(parties);
+}
+
+
+getParties();
